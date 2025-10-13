e50e70064121aaf0091523f0bc64ffafbf643168
Professor <luciano.souza@fatcursos.org.br>
2025-10-13 11:39:28 -0300

    themes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 }
 
 dependencies {
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.core:core-ktx:1.17.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
