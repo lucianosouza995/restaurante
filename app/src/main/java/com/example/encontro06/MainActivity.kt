58c54811b288812db85727f0044aee0cb55cdb83
Professor <luciano.souza@fatcursos.org.br>
2025-10-14 11:57:47 -0300

    Botao Sobremesas

--- app/src/main/java/com/example/encontro06/MainActivity.kt
+++ app/src/main/java/com/example/encontro06/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.example.encontro06
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.chip.Chip
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val chipSobremesa: Chip = findViewById(R.id.chip_sobremesa)
+
+        chipSobremesa.setOnClickListener {
+            val intent = Intent(this, SobremesaActivity::class.java)
+
+            startActivity(intent)
         }
     }
 }
