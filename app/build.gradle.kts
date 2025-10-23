1b2a2f7d99ac1dddf45f3904ae3c1e5459727924
Professor <luciano.souza@fatcursos.org.br>
2025-10-23 08:45:45 -0300

    Glibe

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -41,6 +42,8 @@ android {
 }
 
 dependencies {
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    kapt("com.github.bumptech:compiler:4.12.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.core:core-ktx:1.17.0")
     implementation(libs.androidx.core.ktx)
--- app/src/main/java/com/example/encontro06/SobremesaAdapter.kt
+++ app/src/main/java/com/example/encontro06/SobremesaAdapter.kt
@@ -1,7 +1,60 @@
 package com.example.encontro06
 
-class SobremesaAdapter {
-    private val desserts: List<Sobremesa>
-    private val onQuantityChanged:(Sobremesa)
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
+import java.text.NumberFormat
+import java.util.Locale
+import com.bumptech.glide.Glide
+
+class SobremesaAdapter (
+    private val desserts: List<Sobremesa>,
+    private val onQuantityChanged:(Sobremesa) -> Unit
+    ) : RecyclerView.Adapter<SobremesaAdapter.DessertViewHolder>() {
+
+    inner class DessertViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val name: TextView = itemView.findViewById(R.id.textViewName_2)
+        val price: TextView = itemView.findViewById(R.id.textViewPrice_2)
+        val description: TextView = itemView.findViewById(R.id.textViewDescription_2)
+        val quantity: TextView = itemView.findViewById(R.id.textViewQuantity_2)
+        val itemTotal: Text = itemView.findViewById(R.id.textViewItemTotal_2)
+        val image: ImageView = itemView.findViewById(R.id.imageViewDessert_2)
+        val buttonPlus: ImageButton = itemView.findViewById(R.id.buttonPlus_2)
+        val buttonMinus: ImageButton = itemView.findViewById(R.id.buttonMinus_2)
+        val button: Button = itemView.findViewById(R.id.buttonAddToCart_2)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DessertViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.lista_item_sobremesa, parent, false)
+        return DessertViewHolder(view)
+    }
+
+
+    override fun getItemCount(): Int {
+        return desserts.size
+
+    }
+
+    override fun onBindViewHolder(holder: SobremesaAdapter.DessertViewHolder, position: Int) {
+        val dessert = desserts[position]
+        val locale = Locale("pt","BR")
+        val currencyFormat = NumberFormat.getCurrencyInstance(locale)
+
+        holder.name.text = dessert.name
+        holder.price.text = "Preço ${currencyFormat.format(dessert.price)}"
+        holder.description.text = dessert.description
+
+        Glide.with(holder.itemView.context)
+
+
+        }
+
 
 }
