c7dee6cc9909650be5305cf8b5ad96512eb8fb94
Professor <luciano.souza@fatcursos.org.br>
2025-10-31 08:16:39 -0300

    Tirar foto e salvar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,11 @@ android {
 }
 
 dependencies {
+    val camerax_version = "1.3.0"
+    implementation("androidx.camera:camera-core:${camerax_version}")
+    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
+    implementation("androidx.camera:camera-view:${camerax_version}")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     kapt("com.github.bumptech.glide:compiler:4.12.0")
     implementation("com.google.android.material:material:1.12.0")
--- app/src/main/java/com/example/encontro06/CadastroProdutoActivity.kt
+++ app/src/main/java/com/example/encontro06/CadastroProdutoActivity.kt
@@ -1,10 +1,32 @@
 package com.example.encontro06 // Adjust package name if needed
 
+import android.Manifest
+import android.content.ContentValues
+import android.content.pm.PackageManager
+import android.icu.text.SimpleDateFormat
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.textfield.TextInputEditText // Import for TextInputEditText
+import java.util.Locale
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CadastroProdutoActivity : AppCompatActivity() {
 
@@ -15,23 +37,163 @@ class CadastroProdutoActivity : AppCompatActivity() {
     private lateinit var editTextProductQuantity: TextInputEditText
     private lateinit var buttonRegisterProduct: Button
 
+    private lateinit var cameraExecutor: ExecutorService
+
+    private lateinit var previewView: PreviewView
+    private lateinit var imageView: ImageView
+    private lateinit var buttonTirarFoto: Button
+    private lateinit var toolbar : androidx.appcompat.widget.Toolbar
+
+    private var imageCapture: ImageCapture? = null
+
+    private var saveUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro_produto) // Link to the XML layout
 
         // Initialize views using findViewById
+        previewView = findViewById(R.id.previewView)
+        cameraExecutor = Executors.newSingleThreadExecutor()
         editTextProductName = findViewById(R.id.editTextProductName)
         editTextProductDescription = findViewById(R.id.editTextProductDescription)
         editTextProductPrice = findViewById(R.id.editTextProductPrice)
         editTextProductQuantity = findViewById(R.id.editTextProductQuantity)
         buttonRegisterProduct = findViewById(R.id.buttonRegisterProduct)
+        buttonTirarFoto = findViewById(R.id.buttonTirarFoto)
+        imageView = findViewById(R.id.imageViewFoto)
 
         // Set action for the button click
         buttonRegisterProduct.setOnClickListener {
             registerProduct()
         }
+        buttonTirarFoto.setOnClickListener {
+            if (imageView.visibility == View.VISIBLE) {
+                imageView.visibility = View.GONE
+                previewView.visibility = View.VISIBLE
+                saveUri = null
+            }
+            else if (imageCapture == null){
+                requestCameraPermission()
+            }
+            else {
+                    tirarFoto()
+
+            }
+        }
+        toolbar = findViewById(R.id.toolbarCadastro)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+
+
+    }
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // Permissão concedida. Inicie a câmera.
+            startCamera()
+        } else {
+            // Permissão negada. Informe o usuário.
+            Toast.makeText(this, "Permissão da câmera é necessária", Toast.LENGTH_SHORT).show()
+        }
     }
 
+
+    private fun requestCameraPermission() {
+        when {
+            ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED -> {
+                // Permissão já concedida. Inicie a câmera.
+                startCamera()
+            }
+            else -> {
+                // Permissão não concedida. Solicite ao usuário.
+                requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+            }
+        }
+    }
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder().build().also {
+                it.setSurfaceProvider(previewView.surfaceProvider)
+            }
+
+            imageCapture = ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                .build()
+
+            // Selecione a câmera traseira
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Desvincule todos os casos de uso
+                cameraProvider.unbindAll()
+
+                // Vincule o caso de uso de visualização à câmera
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+            } catch(exc: Exception) {
+                // Tratar erros
+                Toast.makeText(this, "Falha ao iniciar a câmera", Toast.LENGTH_SHORT).show()
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+
+    }
+
+    private fun tirarFoto(){
+        val imageCapture = imageCapture ?: run {
+            Log.e("tirarFoto","ImageCapture é nulo. A câmera foi inicializada?")
+            return
+        }
+        val name = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US)
+            .format(System.currentTimeMillis())
+
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P)
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/Encontro06-Imagens")
+        }
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(
+                contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+            )
+            .build()
+
+        imageCapture.takePicture(
+            outputOptions, // A variável que você criou acima com os ContentValues que dirá onde gravar
+            ContextCompat.getMainExecutor(this), // Onde o callback será executado
+            object : ImageCapture.OnImageSavedCallback { // O callback para saber o resultado
+
+                // Chamado em caso de SUCESSO
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    saveUri = output.savedUri // Guarda a URI da imagem salva
+                    val msg = "Foto salva com sucesso: $saveUri"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d("tirarFoto", msg)
+
+                    // ATUALIZA A UI: Mostra a foto e esconde o preview
+                    imageView.setImageURI(saveUri)
+                    imageView.visibility = View.VISIBLE
+                    previewView.visibility = View.GONE
+                    buttonTirarFoto.text = "Tirar Outra Foto"
+                    }
+
+                // Chamado em caso de ERRO
+                    override fun onError(exc: ImageCaptureException) {
+                        Log.e("tirarFoto", "Falha ao capturar foto: ${exc.message}", exc)
+                        Toast.makeText(baseContext, "Falha ao salvar foto.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            )
+        }
     private fun registerProduct() {
         // Get text from input fields
         val productName = editTextProductName.text.toString().trim()
