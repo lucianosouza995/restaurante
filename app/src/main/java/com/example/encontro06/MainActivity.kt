156fc1704b50433686ef02832c6a7c8598783c87
Professor <luciano.souza@fatcursos.org.br>
2025-11-07 10:22:42 -0300

    firestore completo, com botão sair

--- app/src/main/java/com/example/encontro06/MainActivity.kt
+++ app/src/main/java/com/example/encontro06/MainActivity.kt
@@ -2,17 +2,23 @@ package com.example.encontro06
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.chip.Chip
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
+
+        auth = FirebaseAuth.getInstance()
 
         val chipSobremesa: Chip = findViewById(R.id.chip_sobremesa)
 
@@ -21,5 +27,19 @@ class MainActivity : AppCompatActivity() {
 
             startActivity(intent)
         }
+        val buttonLogout: Button = findViewById(R.id.buttonLogout)
+
+        buttonLogout.setOnClickListener {
+            performLogout()
+        }
+    }
+    private fun performLogout() {
+        auth.signOut()
+
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+        finish()
+
     }
 }
